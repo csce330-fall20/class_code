bdfa6d278ddd3c9043362afaf29e16c2c508135e
student <oreillyj@email.sc.edu>
2020-10-15 08:19:34 -0700

    10/15

--- s001/midterm.pl
+++ s001/midterm.pl
@@ -4,8 +4,6 @@
 
 % Also IMPORTANT: Do not forget the online quiz (20%).
 
-:-['Tests.pl'].
-
 % (20%) Section 1. Given a Knowledge Base with this _structure_
 %  write queries to answer the following queries.
 %  IMPORTANT: your queries should be sufficiently general 
@@ -16,7 +14,6 @@
 
 %State KB regarding employees
 % assume it is complete, but not all facts given
-
 %entity type is a state, county, or city
 %entity(id,type,name)
 entity(id00000,state,sc). %only one for SC
@@ -26,9 +23,7 @@ entity(id00023,county,lexington).
 entity(id04321,city,columbia).
 entity(id01321,city,greenville).
 %....
-entity(id05555,city,yemassee).
 
-employee(id05555, mayor, 'Yem Mayor',    555555555,  2011, -1).
 % Employees:
 % 
 %     employment end will be -1 if still in position,
@@ -40,27 +35,23 @@ employee(id00000, governor, 'Henry McMaster', 059105912, 2017, -1).
 employee(id04321, mayor, 'Steve Benjamin',    602950513,  2010, -1).
 employee(id04321, mayor, 'Bob Coble',         186038354,   1990, 2010).
 %...
-
+employee(id00023, sheriff, 'Leon Lott',       510312643,   1996, -1).
 %...
+entity(id05555,city,yemassee).
 
+employee(id05555, mayor, 'Yem Mayor',    555555555,  2011, -1).
 %1a Who is the current mayor of yemassee ( a city in SC ), give the name.
-mayor_of_yemassee(Name) :- entity(ID,city,yemassee), employee(ID,mayor,Name,_,_,-1).
+mayor_of_yemassee(Name) :- fail.
 
 %1b What other jobs has the current Governor of SC held.
-% for full credit, you must not assume that McMaster is the current governor, i.e.
+% for full credit, you must not assume that McMaster is the current givernor, i.e.
 % your query should work 20 years from now.
 % You don't need to give all the jobs at once (user may press ';').
-current_governor_past_jobs(Job_Title) :- 
-    employee(id00000, governor, _, EmpID, _, -1),
-    employee(id00000, Job,      _, EmpID, _, EndDate),EndDate > -1.
-
+current_governor_past_jobs(Job_Title) :- fail.
 
 %1c Is anyone currently holding two or more jobs under different entities. Give 
 % the name and the employee_id.
-two_jobs_two_places(Name, EmpID) :- 
-    employee(ID1, _, Name,       EID,   _, _),
-    employee(ID2, _, Name,       EID,   _, _),
-    \+ ID1 = ID2. 
+two_jobs_two_places(Name, EmpID) :- fail.
 
 % (20%) Section 2: Write your Fall '20 Schedule. If you have fewer than 12 hours 
 %     (~4 classes), pad your schedule with made up BKWG (Basketweaving)
@@ -70,32 +61,11 @@ two_jobs_two_places(Name, EmpID) :-
 %     necessary (e.g., you are graduating this summer).
 %   2.a (10%) Your Schedule (make sure it can be used to solve 2b and 2c)
 
-course(csce, 330, [t,r],  1315, 1430).
-course(csce, 350, [t,r],  1315, 1430).
-course(bwng, 220, [m,w],  1535, 1625).
-course(bwng, 110, [t,r],  0830, 0945).
-
 %    2.b (5%) What non-CSCE courses are you taking? (again, the user may use 
 %       ';' to get them all)
-non_csce( [C,N] ) :- course(C,N,_,_,_).
+non_csce(Course) :- fail.
 %    2.c (5%) Do any of your classes overlap in time and day
-
-overlapped_courses(CN1, CN2 ):-
-    course(C1,N1,Days1,Time1,End1),
-    course(C2,N2,Days2,Time2,End2),
-    member(D,Days1),member(D,Days2),
-    overlapped_times(Time1,End1,Time2,End2),
-    CN1 =[C1,N1],
-    CN2 = [C2,N2],
-    \+ CN1 = CN2.
-
-overlapped_times(Time1,End1,Time2,_):-
-    Time2>=Time1,
-    Time2=<End1.
-overlapped_times(Time2,_,Time1,End1):-
-    Time2>=Time1,
-    Time2=<End1.
-
+overlapped_courses(CourseA, CourseB) :-fail.
 
 
 
@@ -144,15 +114,7 @@ node(7,nil,nil).
 %    search the tree for N , if set, and succeed if N is set
 %    if _unset_ should return all members of the tree (one at a time, 
 %    with backtracking or user pressing ';')
-binary_tree_search0(N):- node(N,_,_).
-
-binary_tree_search1(N):- root(R), descendant(N,R), \+ N=nil.
-
-descendant(D,A) :- node(A,A2,_),descendant(D,A2).
-descendant(D,A) :- node(A,D,_).
-descendant(D,D).
-descendant(D,A) :- node(A,_,D).
-descendant(D,A) :- node(A,_,A2),descendant(D,A2).
+binary_tree_search(N) :- fail.
 
 
 %4.b define a rule for path_to_node(N,Path) that returns a list of all the paths 
--- s005/midterm.pl
+++ s005/midterm.pl
@@ -4,8 +4,6 @@
 
 % Also IMPORTANT: Do not forget the online quiz (20%).
 
-:-['Tests.pl'].
-
 % (20%) Section 1. Given a Knowledge Base with this _structure_
 %  write queries to answer the following queries.
 %  IMPORTANT: your queries should be sufficiently general 
@@ -16,7 +14,6 @@
 
 %State KB regarding employees
 % assume it is complete, but not all facts given
-
 %entity type is a state, county, or city
 %entity(id,type,name)
 entity(id00000,state,sc). %only one for SC
@@ -34,33 +31,37 @@ employee(id05555, mayor, 'Yem Mayor',    555555555,  2011, -1).
 %     employment end will be -1 if still in position,
 %     e.g. McMaster is still governor of sc
 %     Employee names are not guaranteed to be unique
-% employee(entity_id, job_title, name, emplyee id, employment_start_year, emplyoment_end_year)
+% employee(entity_id, job_title, name, employee id, employment_start_year, emplyoment_end_year)
 employee(id00000, governor, 'Henry McMaster', 059105912, 2017, -1).
 %...
 employee(id04321, mayor, 'Steve Benjamin',    602950513,  2010, -1).
 employee(id04321, mayor, 'Bob Coble',         186038354,   1990, 2010).
 %...
-
+employee(id00023, sheriff, 'Leon Lott',       510312643,   1996, -1).
 %...
 
 %1a Who is the current mayor of yemassee ( a city in SC ), give the name.
-mayor_of_yemassee(Name) :- entity(ID,city,yemassee), employee(ID,mayor,Name,_,_,-1).
+mayor_of_yemassee(Name) :- 
+    entity(ID,city,yemassee),
+    employee(ID,mayor,Name,_,_,-1).
 
 %1b What other jobs has the current Governor of SC held.
-% for full credit, you must not assume that McMaster is the current governor, i.e.
+% for full credit, you must not assume that McMaster is the current givernor, i.e.
 % your query should work 20 years from now.
 % You don't need to give all the jobs at once (user may press ';').
 current_governor_past_jobs(Job_Title) :- 
-    employee(id00000, governor, _, EmpID, _, -1),
-    employee(id00000, Job,      _, EmpID, _, EndDate),EndDate > -1.
+    entity(StateID,state,sc),
+    employee(StateID,governor,_,EID,_,-1),
+    employee(_,    Job_Title ,_,EID,_,End),
+    End > -1.
 
 
 %1c Is anyone currently holding two or more jobs under different entities. Give 
 % the name and the employee_id.
 two_jobs_two_places(Name, EmpID) :- 
-    employee(ID1, _, Name,       EID,   _, _),
-    employee(ID2, _, Name,       EID,   _, _),
-    \+ ID1 = ID2. 
+    employee(ID1,_,Name,EmpID,_,-1),
+    employee(ID2,_,Name,EmpID,_,-1),
+    \+ ID1 = ID2.
 
 % (20%) Section 2: Write your Fall '20 Schedule. If you have fewer than 12 hours 
 %     (~4 classes), pad your schedule with made up BKWG (Basketweaving)
@@ -70,32 +71,11 @@ two_jobs_two_places(Name, EmpID) :-
 %     necessary (e.g., you are graduating this summer).
 %   2.a (10%) Your Schedule (make sure it can be used to solve 2b and 2c)
 
-course(csce, 330, [t,r],  1315, 1430).
-course(csce, 350, [t,r],  1315, 1430).
-course(bwng, 220, [m,w],  1535, 1625).
-course(bwng, 110, [t,r],  0830, 0945).
-
 %    2.b (5%) What non-CSCE courses are you taking? (again, the user may use 
 %       ';' to get them all)
-non_csce( [C,N] ) :- course(C,N,_,_,_).
+non_csce(Course) :- fail.
 %    2.c (5%) Do any of your classes overlap in time and day
-
-overlapped_courses(CN1, CN2 ):-
-    course(C1,N1,Days1,Time1,End1),
-    course(C2,N2,Days2,Time2,End2),
-    member(D,Days1),member(D,Days2),
-    overlapped_times(Time1,End1,Time2,End2),
-    CN1 =[C1,N1],
-    CN2 = [C2,N2],
-    \+ CN1 = CN2.
-
-overlapped_times(Time1,End1,Time2,_):-
-    Time2>=Time1,
-    Time2=<End1.
-overlapped_times(Time2,_,Time1,End1):-
-    Time2>=Time1,
-    Time2=<End1.
-
+overlapped_courses(CourseA, CourseB) :-fail.
 
 
 
@@ -120,7 +100,7 @@ overlapped_times(Time2,_,Time1,End1):-
 %    > 'nil' is not a "regular" member of the tree and should not be returned.
 
 %EXAMPLE tree
-root(5).
+%root(5).
 
 node(5,3,8).
 node(3,1,4).
@@ -130,6 +110,7 @@ node(0,nil,nil).
 node(8,7,nil).
 node(7,nil,nil).
 
+root(R) :- node(R,_,_), \+ (node(_,R,_);node(_,_,R)).
 %above is tree 
 %                   5
 %                 /   \
@@ -144,17 +125,25 @@ node(7,nil,nil).
 %    search the tree for N , if set, and succeed if N is set
 %    if _unset_ should return all members of the tree (one at a time, 
 %    with backtracking or user pressing ';')
-binary_tree_search0(N):- node(N,_,_).
+node(N) :- node(N,_,_).
 
-binary_tree_search1(N):- root(R), descendant(N,R), \+ N=nil.
+binary_tree_search(N) :- root(Current), bts_helper(N,Current).
 
-descendant(D,A) :- node(A,A2,_),descendant(D,A2).
-descendant(D,A) :- node(A,D,_).
-descendant(D,D).
-descendant(D,A) :- node(A,_,D).
-descendant(D,A) :- node(A,_,A2),descendant(D,A2).
+bts_helper(N,Current):-
+    N<Current,
+    node(Current,L,_),
+    \+ L= nil,
+    bts_helper(N,L).
+bts_helper(N,N).
+bts_helper(N,Current):-
+    N>Current,
+    node(Current,_,R),
+    \+ R= nil,
+    bts_helper(N,R).
 
-
+binary_tree_search1(N) :-
+    path_to_node(N,Path),
+    member(N,Path).
 %4.b define a rule for path_to_node(N,Path) that returns a list of all the paths 
 %    from root to a node. As Examples:
 %    path_to_node(5,[5]).
@@ -164,8 +153,20 @@ descendant(D,A) :- node(A,_,A2),descendant(D,A2).
 %    This must be a general solution, i.e. it must work for ALL BSTs 
 %    structured as above.
 
+%DOWN the tree
+path_to_node(N,Path):-root(R),node(N),ptn(N,R,Path).
 
+ptn(N,N,[N]).
+ptn(N,C, [C|PtN] ) :- N<C, node(C,L,_), \+L=nil, ptn(N,L,PtN) .
+ptn(N,C, [C|PtN] ) :- N>C, node(C,_,R), \+R=nil, ptn(N,R,PtN) .
 
+%UP the tree
+path_to_node1(R,[R]) :- root(R).
+path_to_node1(N, Path2N) :-
+    (node(P,N,_); node(P,_,N)),
+    path_to_node(P,P2P),
+    \+ N = nil,
+    append(P2P,[N],Path2N).
 
 
 
