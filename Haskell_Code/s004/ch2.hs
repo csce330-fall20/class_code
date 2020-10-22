a204f71a22017aa3ae040070076f7ba1807d480e
student <oreillyj@email.sc.edu>
2020-10-22 11:32:13 -0700

    10/22

--- Haskell_Code/s004/ch2.hs
+++ Haskell_Code/s004/ch2.hs
@@ -1,4 +1,24 @@
-N = a ’div’ length xs
+n = a `div` length xs
     where
        a = 10
-      xs = [1,2,3,4,5]
+       xs = [1,2,3,4,5]
+
+last' xs = xs !! (length xs -1)
+
+last'' xs = head (reverse xs)
+
+last''' = head.reverse
+
+last'''' xs = drop (length xs -1) xs !! 0
+
+last''''' [x] = x
+last''''' (x:xs) = last''''' xs
+
+init' xs = take (length xs -1) xs
+
+init'' xs = reverse( tail(reverse xs) )
+
+init''' = reverse.tail.reverse
+
+init'''' [x] = []
+init'''' (x:xs) = x:init'''' xs
---
+++ Haskell_Code/s004/ch2_cli.hs
@@ -0,0 +1,7 @@
+double x = x + x
+
+quadruple x = double (double x )
+
+factorial n = product [1..n]
+
+average xs = sum xs `div` length xs
